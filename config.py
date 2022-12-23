a232fecdedf073a87b98f5a151566b15e0b05ab9
Vasiliy40 <114063767+Vasiliy40@users.noreply.github.com>
2022-12-23 15:51:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-user_token = 'vk1.a.MI4Xg6ziks6qj6VRbM_GTSvonqqiDL4XxasUtvZbOL9_orSzl5y5FZGZG5LjvLCfB4iyHAiIfuYj7NTVL7Mc__FP_jr5TSUYPR4X_ezoZ2cF_hfeTlsNMqjbJoFtGQBid-MlQ5-xXIU5I-QIf2lvB6OeXLL4RebXkY0wY_XnuWavM7NGbVOHCi5bFGfpssBTg9CldOsL8ymGtJrJTxiFaw'
-community_token = 'vk1.a.y9Xiu-vl22JGIdDGkzN3fqm-v-EbQN2IffplI0-SLlz3Lu8TQ1QZ24eUldTZoC3AfJjAGht04jXpTGCkT1ujL6DqHUSEfFQj1BfyU3lPXT9PQ0e9uYEtKdgBfUYSZkOcwxXEOwX1Lb9KoJcgJE791nXyRahqm8yDy8j-E3PKV2lTOPaOpZwcEJeted5Vk_Kei70AKkM4Xvx_urto_5zmDg'
+user_token = 'токен пользователя'
+community_token = 'токен сообщества'
 
 offset = 0
 line = range(0, 1000)
 
 postgres_host = 'localhost'
 postgres_user = 'postgres'
-postgres_password = '12345678'
+postgres_password = '1111111'
 postgres_db_name = 'postgres'
